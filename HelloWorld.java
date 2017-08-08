802a75feff70b88ac9ba69c422e444544ae03adb
Jones, Ben <bmjones@mitre.org>
2017-08-08 11:55:04 -0400

    added version 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,10 @@
 public class HelloWorld {
+	
+    private static String version = "2";
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        System.out.println("Hello, World. This is version:"+version);
     }
 
 }
